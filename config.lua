1fd9b108d132e2d82895fadcb469f0421a91dd4f
YeeetSK <yeetoliv3r@gmail.com>
2024-05-25 17:24:31 +0200

    Change display distance

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.Framework = 'qb' -- Framework you use - 'qb' / 'esx'
 
 Config.Target = 'ox_target' -- Target type currently only - 'ox_target'
 
-Config.DisplayDistance = 150 -- Distance where the peds get created at
+Config.DisplayDistance = 50 -- Distance where the peds get created at
 
 Config.Cooldown = 1200 -- Cooldown before a player can ask for food or a drink again - in seconds
 
